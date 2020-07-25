f27c952a5f60520dab4d99e8275a1739bb6ac96b
Patryk <patryk.marcisz@gmail.com>
2020-07-25 13:05:18 +0200

    zadanie 8

--- src/main/java/zadania/ex8/Ex8Runner.java
+++ src/main/java/zadania/ex8/Ex8Runner.java
@@ -2,7 +2,7 @@ package zadania.ex8;
 
 public class Ex8Runner {
     public static void main(String[] args) {
-        Parcel parcel = new Parcel();
+        Parcel parcel = new Parcel(10, 10, 10, 10, true);
 
         Validator parcelValidator = new ParcelValidator();
         boolean validate = parcelValidator.validate(parcel);
