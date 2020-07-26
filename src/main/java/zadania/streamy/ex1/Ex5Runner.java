fde68b44e4280c5b7c18331d68d5c24753f1bc07
Patryk <patryk.marcisz@gmail.com>
2020-07-26 11:58:13 +0200

    zadanie 1.7

--- src/main/java/zadania/streamy/ex1/Ex5Runner.java
+++ src/main/java/zadania/streamy/ex1/Ex5Runner.java
@@ -1,5 +1,34 @@
 package zadania.streamy.ex1;
 
-public class Ex5Runner {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+public class Ex5Runner {
+    public static void main(String[] args) {
+        List<String> collect = Stream.of(1, 26, 30, 2, 45)
+                .filter(element -> element > 26)
+                .map((Integer element) -> {
+                    String result = String.valueOf(element);
+                    return result;
+                })
+                .collect(Collectors.toList());
+
+        for (String str : collect) {
+            System.out.println(str);
+        }
+
+        main2(new String[]{});
+    }
+
+    public static void main2(String[] args) {
+        Stream.of(1, 26, 39, 2, 45)
+                .filter(wejsciowa -> wejsciowa > 26)
+                .map(integer -> {
+                    return integer = new Integer(integer);
+                })
+                .map(integer -> Integer.toString(integer))
+                .forEach(str -> System.out.println(str));
+    }
 }
+
---
+++ src/main/java/zadania/streamy/ex1/Ex7Runner.java
@@ -0,0 +1,43 @@
+package zadania.streamy.ex1;
+
+public class Ex7Runner {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private float height;
+    private String comment;
+
+    public Person(String name, String surname, int age, float height, String comment) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.height = height;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
